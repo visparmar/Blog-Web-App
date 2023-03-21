9e9439d34cc4bea0d58188338f7d88a5bb3f228d
vikas <visparmar119@gmail.com>
2023-03-21 23:17:57 +0530

    creating project

---
+++ src/components/account/Login.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Box ,TextField} from '@mui/material';
+
+const Login = () => {
+    return (
+        <>
+        <Box>
+            <img src="https://www.sesta.it/wp-content/uploads/2021/03/logo-blog-sesta-trasparente.png" alt="this is logo" />
+<TextField variant="standard"/>
+<TextField variant="standard"/>
+        </Box>
+        
+        </>
+    );
+}
+
+
+
+export default Login;
