b8725f266c64e14cf59b965febac3b41032356b7
vikas <visparmar119@gmail.com>
2023-03-25 18:15:01 +0530

    create Landing Page && Login Page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Login from './components/account/Login'
+import Landing from './LandingPage/Landing'
 
 function App() {
   return (
-    <div className="App">
-      <Login/>
+    <Router>
+ <div className="App">
+  <Routes>
+    <Route path='/' element={<Landing/>}/>
+    <Route path='Our Story' element=''/>
+    <Route path='Membership' element=''/>
+    <Route path='Write' element=''/>
+    <Route path='Sign_In' element={<Login/>}/>
+       
+    
+  </Routes>
+      
+      
     </div>
+    </Router>
+   
   );
 }
 
--- src/components/account/Login.js
+++ src/components/account/Login.js
@@ -1,15 +1,62 @@
-import React from 'react';
-import { Box ,TextField} from '@mui/material';
+import React,{useState}from 'react';
+import { TextField, Button, } from '@mui/material';
+import './Login.css'
+
+
 
 const Login = () => {
+
+const [account,setaccount]=useState('login');
     return (
-        <>    
-        <Box>
-            <img src="https://www.sesta.it/wp-content/uploads/2021/03/logo-blog-sesta-trasparente.png" alt="this is logo" />
-<TextField variant="standard"/>
-<TextField variant="standard"/>
-        </Box>
-          
+        <>
+             
+
+            <div className='container'>
+{
+            account==='login'?
+                <div className="inner-container">
+
+                    <div className="login-image">
+                        <img src="https://www.sesta.it/wp-content/uploads/2021/03/logo-blog-sesta-trasparente.png" alt="this is logo" />
+                    </div>
+                    <div className="info-input">
+
+
+                        <TextField variant="standard" className='txtfield' label='Enter Username' />
+                        <TextField variant="standard" className='txtfield' label='Enter Password' />
+                    </div>
+                    <div className="button-login">
+                        <Button variant="contained">Login</Button>
+                        <span>OR</span>
+                        <Button className='create' onClick={()=>setaccount('create')}>Create an Account</Button>
+                    </div>
+
+
+                </div>
+ :
+                <div className="inner-container">
+
+                    <div className="login-image">
+                        <img src="https://www.sesta.it/wp-content/uploads/2021/03/logo-blog-sesta-trasparente.png" alt="this is logo" />
+                    </div>
+                    <div className="info-input">
+
+                    <TextField variant="standard" className='txtfield' label='Enter Name' />
+                        <TextField variant="standard" className='txtfield' label='Enter Username' />
+                        <TextField variant="standard" className='txtfield' label='Enter Password' />
+                    </div>
+                    <div className="button-login">
+                        <Button variant="contained">Login</Button>
+                        <span>OR</span>
+                        <Button className='create' onClick={()=>setaccount('login')}>Already have an Account</Button>
+                    </div>
+
+
+                </div>
+}
+
+            </div>
+
         </>
     );
 }
